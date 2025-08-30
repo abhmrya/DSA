64d248202354a7a76d3114fc404f7796345b5278
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-08-30 14:32:44 +0530

    student

---
+++ java/student.java
@@ -0,0 +1,19 @@
+public class student{
+    // rollNo       given error this is global assisgn
+    int rollNo  = 22;   // attributes,instance variable(class level variavles)
+
+    void display(){
+        int a;
+        a  = 2;
+        System.out.println("roll no is: " + rollNo);
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        //  System.out.println(rollNo);   //not print directly
+        student obj = new student();
+        System.out.println(obj.rollNo);
+        obj.display();
+    }
+
+} 
