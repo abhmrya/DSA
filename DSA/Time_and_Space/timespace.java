8971adb2f31b422f7f5743db02761da0fcee4bb3
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-10-01 11:19:14 +0530

    time

---
+++ DSA/Time_and_Space/timespace.java
@@ -0,0 +1,28 @@
+package DSA.Time_and_Space;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class timespace {
+    static void linearsearch(){
+        int x = 5;
+        long starttime,elapsedtime;
+        int[] arr =  new int[] {10,7,-4,28,50};  // (10,7),(10,-4),(10,20),(10,50),(7,10)
+        // int [] arr= new int[500303090];
+        // Arrays.fill(arr,10);
+        starttime = System.nanoTime();
+        for (int i=0;i<arr.length;i++){   // O(n)
+            if(arr[i]==x){
+                System.out.println("Element found");
+            }
+        }
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+        }
+        elapsedtime = System.nanoTime()-starttime;
+        System.out.println("Elapsed Time :"+ elapsedtime/1000+"milisecound");
+    }
+    public static void main(String[] args) {
+        linearsearch();   //O(n+100)
+    }
+}
