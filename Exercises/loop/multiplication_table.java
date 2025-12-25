a1e329caf91baf3f50180df718705133e81cce0a
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-12-25 17:17:12 +0530

    table

---
+++ Exercises/loop/multiplication_table.java
@@ -0,0 +1,14 @@
+package Exercises.loop;
+
+import java.util.Scanner;
+
+public class multiplication_table {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your table number: ");
+        int n = scanner.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+ " X "+ i+ " = " + n*i);
+        }
+    }
+}
