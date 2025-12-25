febfdd1d56197a8a1c4045cf498dcc9c00aa89be
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-12-25 16:54:02 +0530

    sum

---
+++ Exercises/loop/sum_first_n_number.java
@@ -0,0 +1,16 @@
+package Exercises.loop;
+
+import java.util.Scanner;
+
+public class sum_first_n_number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the n number");
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i=0;i<=n;i++){
+            sum= sum+i;
+        }    
+        System.out.println("you given number of sum n number is "+ sum );
+    }
+}
