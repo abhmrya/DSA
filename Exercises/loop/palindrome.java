4e91d3dd2e4fcd008167c47e6432cc58349b228f
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-12-26 23:29:59 +0530

    palindrome

---
+++ Exercises/loop/palindrome.java
@@ -0,0 +1,21 @@
+package Exercises.loop;
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the word or number.");
+        String str  = scanner.nextLine();
+        int n = str.length();
+        String rev="";
+        for(int i = n-1 ;i>=0;i--){
+            rev += str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equel");
+        }
+
+    }
+}
