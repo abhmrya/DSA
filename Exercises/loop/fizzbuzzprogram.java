b1a3d4be3f83ce5931ca8cab0be9c022043fb74a
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-12-27 00:26:14 +0530

    fizzbuss

---
+++ Exercises/loop/fizzbuzzprogram.java
@@ -0,0 +1,24 @@
+package Exercises.loop;
+
+import java.util.Scanner;
+
+public class fizzbuzzprogram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=1;i<=n;i++){
+            if (i%3==0 && i%5==0){
+                System.out.println("fizzbuzz");
+            }
+            else if (i%3==0) {
+                System.out.println("fizz");
+            } 
+            else if(i%5==0) {
+                System.out.println("buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
