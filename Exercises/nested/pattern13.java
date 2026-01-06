61bf1cb657a7578fe020c988f909d5bec1eb6d07
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-06 15:00:42 +0530

    pattern 13

---
+++ Exercises/nested/pattern13.java
@@ -0,0 +1,30 @@
+package Exercises.nested;
+
+import java.util.Scanner;
+
+public class pattern13 {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Enter the number.");
+        int n= scanner.nextInt();
+        for (int i = 1; i <=n/2+1; i++) {
+            for (int j = 1; j <=n/2+1-i; j++) {
+                System.out.print("  "); 
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <=n/2; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("  "); 
+            }
+            for (int j = 1; j <=n/2-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
