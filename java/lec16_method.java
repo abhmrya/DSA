b15b88480d073496ce6fb8bc09ddb6cf035e7ee5
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-06 13:49:23 +0530

    pattern 12

--- java/lec16_method.java
+++ java/lec16_method.java
@@ -1,23 +1,64 @@
-import java.util.Scanner;
 
 public class lec16_method {
-    static int addition(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = scanner.nextInt();
-        System.out.println("Enter secound number:");
-        int num2 = scanner.nextInt();
-        int sum = num1+num2;
-        System.out.println("The sum is: "+sum);
-        scanner.close();
-        return sum;
+// t-1
+    // static int addition(){
+        // Scanner scanner = new Scanner(System.in);
+    //     System.out.println("Enter first number:");
+    //     int num1 = scanner.nextInt();
+    //     System.out.println("Enter secound number:");
+    //     int num2 = scanner.nextInt();
+    //     int sum = num1+num2;
+    //     // System.out.println("The sum is: "+sum);
+    //     scanner.close();
+    //     return sum;
+    // }
+// t-2
+    //with return type and with argument
+    // static int addition(int a,int b){
+    //     int sum = a+b;
+    //     return sum;
+    // }
+// t-3
+    //     //without return type and with argument
+    // static void addition(int a,int b){
+    //     int sum = a+b;
+    //     System.out.println("sum is : "+sum);
+    // }
+// t-4
+        //without return type and with argument
+    // static void addition(int a,double b){
+    //     double sum = a+b;
+    //     System.out.println("sum is : "+sum);
+    // }
+
+    static String greetMessage(){
+        boolean isMorning=true;
+        if(!isMorning){
+            return "good morning";
+        }
+        else{
+            return "Hare Krishna";
+        }
     }
     public static void main(String[] args) {
-
-        int a = addition();
-        System.out.println(a);
+        String result = greetMessage();
+        System.out.println(result);
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter first number:");
+        // int a = scanner.nextInt();
+        // System.out.println("Enter secound number:");
+        // int b = scanner.nextInt();
+// t-2
+        // int result = addition(a,b);
+        // System.out.println("The sum is:"+result);
+// t-3
+        // addition(a,b);
+// t-4
+        // addition((int) 7.7f,9);
+// t-1
         // addition();
         // addition();
+        // scanner.close();
     
     }
 }
