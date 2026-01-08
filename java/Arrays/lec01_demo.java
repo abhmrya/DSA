aad3475d649bc0e059f50ec02dde53df5cdd3a2f
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-08 18:02:24 +0530

    array demo

---
+++ java/Arrays/lec01_demo.java
@@ -0,0 +1,70 @@
+package Arrays;
+
+
+public class lec01_demo {
+    static int[] marks;
+    // static double[] marks;
+    public static void main(String[] args) {
+        // marks = new int[5];
+        // marks = new double[5];
+        // System.out.println(marks);
+        // marks = int[]{1,2,3,4,5};  //error
+        marks =new int[]{1,2,3,4,5};
+        
+        //Declaration of an array
+        // double[] marks;  // array are object (not memmory allocation)
+
+        // double[] marks = new double[5];  // instantiaton of an array
+        // ********* or  ***********
+        // double[] marks;
+        // marks = new double[5];
+
+        // double[] marks  = new double[]{34.6,98,99.8};  //{...} aayay initializar
+        // ***** or   ******
+        // double[] marks = {22,32.4,323,3};
+
+        // double[] marks;
+        // marks = {34,78,99.5,89};
+        // double[] marks = {43,54,65,76};  both are given error
+
+        // double[] marks;
+        // marks = new double[]{34.5,89,99.4};
+
+        // double[] marks = new double[5];
+        // marks[0]=5;
+        // marks[1]=6;
+        // marks[2]=7;
+        // marks[3]=8;
+        // // marks[4]=9;  
+        // System.out.println(marks[1]); 
+        // System.out.println(marks[4]);
+        // System.out.println(marks.length);
+        // int arraytlength = marks.length;
+        // System.out.println(marks[arraytlength-1]);
+
+        // int n;
+        // double[] marks = new double[n]; //error
+
+        // int[] numbers = {1,2,3,7};
+        // int[] numbers = {1,2,3,7.8};  // error
+        // int[] numbers= new int[-4]; //error
+
+        // int[] numbers = {1,2,3,7};
+        // numbers[1]=100;
+        // System.out.println(numbers[1]);
+
+        // int[] numbers = new int[5];
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter the element.");
+
+        // for(int i =0;i<numbers.length;i++){
+        //     numbers[i]=scanner.nextInt();
+        // }
+
+        // for(int i:numbers){
+        //     System.out.print(i);
+        // }
+        // scanner.close();
+    }
+    
+}
