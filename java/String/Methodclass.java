5fd437f0855ad44c66983ea8bec5839efb6cd242
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-13 19:23:22 +0530

    method String pending

---
+++ java/String/Methodclass.java
@@ -0,0 +1,19 @@
+package String;
+
+public class Methodclass {
+    public static void main(String[] args) {
+        // String str = "abhay kush";
+        // System.out.println(str.length());
+        // System.out.println(str.isEmpty());
+
+        // String str2 = " ";
+        // System.out.println(str2.length());
+        // System.out.println(str2.isEmpty());  // fase
+        // System.out.println(str2.isBlank());  // true
+
+        String str3 ="\n";
+        System.out.println(str3.length());  //1
+        System.out.println(str3.isEmpty());    // false
+        System.out.println(str3.isBlank());   // true
+    }
+}
