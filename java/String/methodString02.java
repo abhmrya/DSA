200946a322c406cd5bb28a1c15f5a8f10797c9f3
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-15 17:50:26 +0530

    String method

---
+++ java/String/methodString02.java
@@ -0,0 +1,66 @@
+package String;
+
+public class methodString02 {
+    public static void main(String[] args) {
+        // String str = "Abhay kushwaha";
+        // System.out.println(str.toUpperCase());
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str.endsWith("waha"));
+        // System.out.println(str.substring(4)); y kushwaha
+        // System.out.println(str.substring(-4));  // error
+        // System.out.println(str.substring(1,7));  //bhay k
+        // System.out.println(str);
+        // System.out.println(str.substring(2,43));  // error
+        // System.out.println(str.substring(5,5));   // 
+
+
+        // String filename = "file.pdf";
+        // System.out.println(filename.substring(filename.lastIndexOf(".")+1));
+
+        // String email = "abhaykush432@gmail.com";
+        // System.out.println(email.substring(email.lastIndexOf("@")+1));
+        // System.out.println(email.substring(email.indexOf("@")+1));
+
+        // String str1 = "    abhay kushwaha   ";
+        // String str2 = "abhay kushwaha";
+        // System.out.println(str1.trim());
+        // System.out.println(str1.equals(str2));   //false
+        // System.out.println(str1.trim().equals(str2.trim()));  // true
+
+        // String str1 = "abhay kushwaha";
+        // System.out.println(str1.replace("ab","@"));
+
+        // String words = "hello what are doing brother";
+        // String[] a = words.split(" ");
+        // for(String word : a){
+        //     System.out.println(word);
+        // }
+
+        // String[] a = words.split(" ",2);
+        // for(String word : a){
+        //     System.out.println(word);
+        // }
+
+        // String[] a = words.split("a",2);
+        // for(String word : a){
+        //     System.out.println(word);
+        // }
+
+        int num =5;
+        float f= 67.9f;
+        String strnum = String.valueOf(num);
+        String strf = String.valueOf(f);
+        System.out.println(strnum+strf);
+        System.out.println(num+10);
+        System.out.println(strnum+10);
+        // System.out.println(String.valueOf(num));
+        // System.out.println(String.valueOf(f));
+
+
+
+        
+
+
+
+    }
+}
