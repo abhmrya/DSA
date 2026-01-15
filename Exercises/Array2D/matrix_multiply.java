a9d4d74157a0759ad2d9a58b4dec365f50e9dd79
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-01-15 11:50:44 +0530

    matrix

--- Exercises/Array2D/matrix_multiply.java
+++ Exercises/Array2D/matrix_multiply.java
@@ -4,41 +4,46 @@ import java.util.Scanner;
 
 public class matrix_multiply {
     public static void main(String[] args) {
-        int[][] matrix = new int[3][3];
-        int[][] matrix2 = new int[3][3];
-        int[][] multiply = new int[3][3];
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first matrix element 3X3");
+        int[][] matrix1 = new int[3][3];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                matrix1[i][j]=scanner.nextInt();
+            }  
+        }
 
-        // input of matrix
-        System.out.println("Enter the element of 1st matrix.");
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                matrix[i][j]=scanner.nextInt();
-            }
-        }
-        System.out.println("Enter the element of 2st matrix.");
-        for(int i=0;i<matrix2.length;i++){
-            for(int j=0;j<matrix2[i].length;j++){
+        System.out.println("Enter the second matrix element 3X3");
+        int[][] matrix2 = new int[3][3];
+        for (int i = 0; i < matrix2.length; i++) {
+            for (int j = 0; j < matrix2[i].length; j++) {
                 matrix2[i][j]=scanner.nextInt();
+            }  
+        }
+
+//***********  multiply matrix *********
+        int[][] matrix3 = new int[3][3];
+        for (int i = 0; i < matrix3.length; i++) {
+            for (int j = 0; j < matrix3[i].length; j++) {
+                int multiplymatric=0;
+                for (int k = 0; k < matrix3.length; k++) {
+                    multiplymatric=multiplymatric+matrix1[i][k]*matrix2[k][j];
+                }
+                matrix3[i][j]=multiplymatric;
             }
         }
-        //multiply matrix
-        int a =0;
-        int b=0;
-        System.out.println("Multiply matrix is: ");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                a=0;
-                a = matrix[i][j];
-            } 
+
+// ****** print matrix  *****
+        for (int i = 0; i < matrix3.length; i++) {
+            System.out.print(" [");
+            for (int j = 0; j < matrix3.length; j++) { 
+                System.out.print(matrix3[i][j]+" ");
+            }
+            System.out.print(" ]");
+            System.out.println();
+            
         }
-        for (int i = 0; i < matrix2[0].length; i++) {
-            for (int j = 0; j < matrix2.length; j++) {
-                b=0;
-                b = matrix2[i][j];
-                multiply[i][j]=a*b;
-            } 
-        }
+
 
         scanner.close();
     }
