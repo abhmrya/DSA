419eb41f8b45bb5f251e8a2702a2957359eb7dbe
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-04-08 22:32:17 +0530

    deque

--- DSA/Deque/DequeUsingCircularArray.java
+++ DSA/Deque/DequeUsingCircularArray.java
@@ -17,33 +17,89 @@ public class DequeUsingCircularArray {
             front=0;
             rear=0;
         }
-        else if(front==0){
-            size=size-1;
-        }
         else{
-            front--;
+            front=((front-1)+size)%size;
         }
+        // else if(front==0){
+        //     front=size-1;
+        // }
+        // else{
+        //     front--;
+        // }
         deque[front]=x;
+    }
+
+    void addRear(int x) throws Exception{
+        if(isFull()) throw new Exception("Can add because deque is full.");
+        if(front==-1){
+            front=0;
+            rear=0;
+        }else if(rear==size-1){
+            rear=0;
+        }else{
+            rear++;
+        }
+        deque[rear]=x;
+    }
+
+    int deleteFront() throws Exception{
+        if(isEmpty()) throw new Exception("Can not dispaly. deque is empty.");
+        int removeElement = deque[front];
+        if(front==rear){
+            front=-1;
+            rear=-1;
+        }else if(front==size-1){
+            front=0;
+        }else{
+            front++;
+        }
+        return removeElement;
+    }
+
+        int deleteRear() throws Exception{
+        if(isEmpty()) throw new Exception("Can not dispaly. deque is empty.");
+        int removeElement = deque[rear];
+        if(front==rear){
+            front=-1;
+            rear=-1;
+        }else if(rear==0){
+            rear=size-1;
+        }else{
+            rear--;
+        }
+        return removeElement;
     }
 
     boolean isFull(){
         return (rear+1==front) || (front==0 && rear==size-1);
     }
+
     boolean isEmpty(){
         return front==-1;
     }
-    void dispaly() throws Exception{
+
+    void display() throws Exception{
         if(isEmpty()) throw new Exception("Can not dispaly. deque is empty.");
         int i=front;
         while(i!=rear){
-            System.out.println(deque[i]+" ");
+            System.out.print(deque[i]+" ");
             i=(i+1)%size;
         }
         System.out.println(deque[rear]);
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         DequeUsingCircularArray deque = new DequeUsingCircularArray(5);
-    }
+        deque.addfront(10);
+        deque.addfront(20);
+        deque.addfront(30);
+        deque.addRear(40);
+        deque.addRear(50);
+        deque.display();
+        System.out.println(deque.deleteFront());
+        deque.display();
+        System.out.println(deque.deleteRear());
+        deque.display();
+    }   
 }
