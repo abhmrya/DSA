f7569e7e649bbd56069bb6e5cbd4e8596c79c2e1
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-05-30 16:30:26 +0530

    constructBstPreIterative

--- DSA/BinarySearchTree/ConstructBSTPreOrderM3.java
+++ DSA/BinarySearchTree/ConstructBSTPreOrderM3.java
@@ -1,13 +1,44 @@
 package DSA.BinarySearchTree;
 
+import java.util.Stack;
+
 public class ConstructBSTPreOrderM3 {
     Node root;
+    
+
+    // recursion Method 
     Node constructBstPre(int[] pre ,int[] i,int upper){
         if(i[0]==pre.length || pre[i[0]]>upper) return null;
         Node root= new Node(pre[i[0]]);
         i[0]++;
         root.left = constructBstPre(pre, i, root.data);
         root.right = constructBstPre(pre, i, upper);
+        return root;
+    }
+
+    // Iterative Approach 
+
+    Node constructBstPreIterative(int[] preorder){
+        Stack<Node> stack = new Stack<>();
+        if(preorder.length==0) return null;
+        Node root = new Node(preorder[0]);
+        stack.push(root);
+        Node temp=root;
+        for(int i=1;i<preorder.length;i++){
+            Node newNode = new Node(preorder[i]);
+            if(stack.peek().data>preorder[i]){
+                stack.peek().left=newNode;
+                temp = newNode;
+                stack.push(newNode);
+            }else{
+                while(!stack.isEmpty() && stack.peek().data<preorder[i]){
+                    temp = stack.pop();
+                }
+                temp.right = newNode;
+                temp = newNode;
+                stack.push(newNode);
+            }
+        }
         return root;
     }
 }
--- DSA/BinarySearchTree/CreateBSTMain.java
+++ DSA/BinarySearchTree/CreateBSTMain.java
@@ -13,15 +13,15 @@ public class CreateBSTMain {
         tree.inOrder(tree.root);
         System.out.println();
 
-        // search element section 
-        System.out.println("************  Search Element of 70 *************");
+        // // search element section 
+        // System.out.println("************  Search Element of 70 *************");
         BSTSearch search = new BSTSearch();
         int element = 70;
         boolean searchElement = search.searchBst(tree.root,element);
         System.out.println("If Element found then True otherwise False : "+searchElement);
 
-        // insert element section
-        System.out.println("*************  Insert Element of 55  ************");
+        // // insert element section
+        // System.out.println("*************  Insert Element of 55  ************");
         BSTInsert insert = new BSTInsert();
         int elemInsert = 55;
         insert.insertBST(tree.root,elemInsert);
@@ -29,8 +29,8 @@ public class CreateBSTMain {
         tree.inOrder(tree.root);
         System.out.println();
 
-        // delete section
-        System.out.println("*************  Delete Element of 40  ************");
+        // // delete section
+        // System.out.println("*************  Delete Element of 40  ************");
         BSTDelete delete = new BSTDelete();
         int elemDelete = 40;
         delete.deleteBst(tree.root,elemDelete);
@@ -38,23 +38,29 @@ public class CreateBSTMain {
         tree.inOrder(tree.root);
         System.out.println();
 
-        // Construct BST Using PreOrder  M-2
-        System.out.println("************ Construct BST Using PreOrder  ***********");
+        // // Construct BST Using PreOrder  M-2
+        // System.out.println("************ Construct BST Using PreOrder  ***********");
         ConstructBSTPreOrderM2 constr = new ConstructBSTPreOrderM2();
-        int[] preorder = {10,5,4,7,11,15,12,20,17};
+        int[] preorder = {10,5,4,7,11,15,12,20,18};
         Node root1 = constr.constructBstPre(preorder,0,preorder.length-1);
         tree.displayTree(root1,0);
         tree.inOrder(root1);
         System.out.println();
 
-        // Construct BST Using PreOrder  M-3
-        System.out.println("************ Construct BST Using PreOrder Method 3 ***********");
+        // // Construct BST Using PreOrder  M-3
+        // System.out.println("************ Construct BST Using PreOrder Method 3 Recursion ***********");
         ConstructBSTPreOrderM3 constrm3 = new ConstructBSTPreOrderM3();
-        int[] preorder3 = {10,5,4,7,11,15,12,20,17};
+        int[] preorder3 = {10,5,4,7,11,15,12,20,18};
         int[] i = {0};
         Node root3 = constrm3.constructBstPre(preorder3,i,Integer.MAX_VALUE);
         tree.displayTree(root3,0);
         tree.inOrder(root3);
+        System.out.println();
+
+        System.out.println("************ Construct BST Using PreOrder Method 3 Recursion ***********");
+        Node rootIterative = constrm3.constructBstPreIterative(preorder3);
+        tree.displayTree(rootIterative,0);
+        tree.inOrder(rootIterative);
         System.out.println();
     }
 }
