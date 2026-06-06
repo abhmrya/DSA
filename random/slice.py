95cad1e1276cd96458e7abf7b22dcf0a1c540652
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-07 01:59:10 +0530

    py slice

---
+++ random/slice.py
@@ -0,0 +1,21 @@
+st = 'abhay'
+print(st[:-1])
+print(st[1:-1])
+print(st[-1:])
+print(st[2:0])
+print(st[-6:-1])
+print(st[-1:-3])
+
+num=[1,2,3,4,5,6,7,8,9,10]
+print(num[::-1])
+print(num[::-2])
+print(num[::1])
+print(num[::2])
+print(num[1::2])  # even
+print(num[1:-3:])
+print(num[:-1:2])
+print(num[-4:-9:])
+print(num[-9:-4:])
+print(num[-4:-9:-1])
+print(num[-4:-9:-2])
+print(num[-9:-4:-1])
