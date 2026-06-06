727d604e21475d671d64a0ed8234be07c6cee608
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-06 12:54:04 +0530

    InsertionSort

---
+++ DSA/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,22 @@
+package DSA.SortingAlgorithms;
+
+import  java.util.Arrays;
+
+public class InsertionSort {
+    static void insertionSort(int[] arr, int n){
+        for(int i=0;i<n;i++){
+            int temp = arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,4,10,1,6,1};
+        insertionSort(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
+}
+}
